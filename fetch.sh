a80dd0d2f5a55f5a8c60336aa1759c44460f3fd5
x268 <0x268h@gmail.com>
2020-02-22 00:41:23 +0300

    changed fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Pull latest master and generate a new content
+
+SCRIPT="$(readlink -f "$0")"
+DIR="$(dirname "$SCRIPT")"
+if [[ ! -d venv/ ]]; then
+    echo "Please init venv in '$DIR'"
+    exit 1
+fi
+
 git pull
-source venv/bin/activate
-pelican -s pelicanconf.py
+source "$DIR/venv/bin/activate"
+pelican -s "$DIR/pelicanconf.py"
